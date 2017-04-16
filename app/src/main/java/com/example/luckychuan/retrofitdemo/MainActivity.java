3caf3be6d3e9c4da997da001d952691ad26e7085
Luckychuan <luckychuan96@163.com>
2017-04-16 21:30:33 +0800

    add 使用Rxjava与retrofit结合的方式下载api

--- app/src/main/java/com/example/luckychuan/retrofitdemo/MainActivity.java
+++ app/src/main/java/com/example/luckychuan/retrofitdemo/MainActivity.java
@@ -12,7 +12,13 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,8 +29,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        requestItem();
+    //     requestItem();
 
+        requestItemByRx();
+
+
+    }
+
+    private void requestItemByRx() {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(("http://www.tngou.net/"))
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+        final ApiService apiService = retrofit.create(ApiService.class);
+
+        apiService.getTabByRx()
+                .flatMap(new Func1<Tab, Observable<Item>>() {
+
+                    @Override
+                    public Observable<Item> call(Tab tab) {
+                        if (tab.isStatus()) {
+                            for (Tab.TabData data : tab.getDataList()) {
+                                Log.d("MainActivity", "onResponse: " + data.toString());
+                            }
+                            return apiService.getItemByRx(tab.getDataList().get(0).getId());
+                        }
+                        return null;
+                    }
+
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.newThread())
+                .subscribe(new Action1<Item>() {
+                    @Override
+                    public void call(Item item) {
+                        if (item.isStatus()) {
+                            for (Item.ItemData data : item.getDataList()) {
+                                Log.d(TAG, "onResponse: " + data.toString());
+                            }
+                        }
+                    }
+                });
 
     }
 
@@ -33,18 +78,39 @@ public class MainActivity extends AppCompatActivity {
                 .baseUrl("http://www.tngou.net/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        ApiService apiService = retrofit.create(ApiService.class);
+        final ApiService apiService = retrofit.create(ApiService.class);
 
         //获取tab
         Call<Tab> call = apiService.getTab();
         call.enqueue(new Callback<Tab>() {
             @Override
             public void onResponse(Call<Tab> call, Response<Tab> response) {
-               Tab tab = response.body();
-                if(tab.isStatus()){
-                    for(Tab.TabData data: tab.getDataList()){
-                        Log.d("MainActivity", "onResponse: "+data.toString());
+                Tab tab = response.body();
+                if (tab.isStatus()) {
+                    for (Tab.TabData data : tab.getDataList()) {
+                        Log.d("MainActivity", "onResponse: " + data.toString());
                     }
+
+                    //获取item
+                    Call<Item> call1 = apiService.getItem(tab.getDataList().get(0).getId());
+                    call1.enqueue(new Callback<Item>() {
+                        @Override
+                        public void onResponse(Call<Item> call, Response<Item> response) {
+                            Item item = response.body();
+                            if (item.isStatus()) {
+                                for (Item.ItemData data : item.getDataList()) {
+                                    Log.d(TAG, "onResponse: " + data.toString());
+
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<Item> call, Throwable t) {
+
+                        }
+                    });
+
                 }
             }
 
@@ -53,30 +119,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-        //获取item
-
-        Call<Item> call1 = apiService.getItem(1);
-        call1.enqueue(new Callback<Item>() {
-            @Override
-            public void onResponse(Call<Item> call, Response<Item> response) {
-                Item item = response.body();
-                if(item.isStatus()){
-                    for(Item.ItemData data : item.getDataList()){
-                        Log.d(TAG, "onResponse: "+data.toString());
-
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Item> call, Throwable t) {
-
-            }
-        });
-
-
-
 
     }
 }
--- app/src/main/java/com/example/luckychuan/retrofitdemo/dao/ApiService.java
+++ app/src/main/java/com/example/luckychuan/retrofitdemo/dao/ApiService.java
@@ -3,6 +3,7 @@ package com.example.luckychuan.retrofitdemo.dao;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import rx.Observable;
 
 /**
  * Created by Luckychuan on 2017/4/10.
@@ -15,6 +16,14 @@ public interface ApiService {
 
     @GET("api/lore/list")
     Call<Item> getItem(@Query("id") int id);
+
+
+    //使用RxJava形式
+    @GET("api/lore/classify")
+    Observable<Tab> getTabByRx();
+
+    @GET("api/lore/list")
+    Observable<Item> getItemByRx(@Query("id") int id);
 
 
 }
